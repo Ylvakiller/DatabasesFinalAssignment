368d0b03ac88ee2451a3239e351b018f6db2dd4a
Ylvakiller <rem.geuze@gmail.com>
2014-03-24 16:45:58 +0100

    succesfully connected towards the database, onto making more fancy stuff

--- src/DatabaseConnections.java
+++ src/DatabaseConnections.java
@@ -8,12 +8,28 @@ import java.sql.SQLException;
  * This class is the only class that has any communication to the database,
  * Other classes may run different methods that will either change data or retrieve data from the database
  */
+
+
+
 public class DatabaseConnections {
 	
+	private final String hostname = "jdbc:mysql://localhost/";
+	private final String dbName = "finalassignment";
+	private final String username = "root";
+	private final String password = "";
+	private Connection con;
 	/*
 	 * This Constructor will connect to the database, Therefore this is the only part that needs changing when switching databases
 	 */
 	public DatabaseConnections(){
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection(hostname + dbName, username, password);	
+		}catch(Exception ex){
+			System.err.println(ex.getMessage());
+			System.out.close();
+		}
+		System.out.println("Succesfully connected");
 		
 	}
 
--- src/MainRunner.java
+++ src/MainRunner.java
@@ -3,7 +3,7 @@ public class MainRunner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		DatabaseConnections connection = new DatabaseConnections();
 	}
 
 }
